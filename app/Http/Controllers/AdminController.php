a761bb5fb22e5527283b9975a1579f37ad9028ce
bllych <billychua56@gmail.com>
2026-08-27 10:39:17 +0700

    Add: students data

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,7 +9,254 @@ class AdminController extends Controller
     public function index()
     {
         $title = "SPPku - Halaman Admin";
+                $students = [
+            [
+                'id' => 1,
+                'name' => 'Alfredy Rudi',
+                'class' => '12 TKJ 1',
+                'tagihan' => 'Rp. 840.000,00',
+                'status' => 'Belum Lunas'
 
+            ],
+            [
+                'id' => 2,
+                'name' => 'Alice Holly Kristy',
+                'class' => '12 AKL',
+                'tuition' => 'Rp. 840.000,00',
+                'status' => 'Lunas'            
+            ],
+            [
+                'id' => 3,
+                'name' => 'Amos Mikhael',
+                'class' => '12 TKJ 2',
+                'tuition' => 'Rp. 840.000,00',
+                'status' => 'Belum Lunas'
+            ],
+            [
+                'id' => 4,
+                'name' => 'Arianto Widodo Putro',
+                'class' => '12 BiD',
+                'tuition' => 'Rp. 840.000,00',
+                'status' => 'Lunas'
+            ],
+            [
+                'id' => 5,
+                'name' => 'Aricks Wijaya',
+                'class' => '12 TKJ 1',
+                'tuition' => 'Rp. 840.000,00',
+                'status' => 'Belum Lunas'
+            ],
+            [
+                'id' => 6,
+                'name' => 'Arthur Sebastian Felix',
+                'class' => '12 TKJ 2',
+                'tuition' => 'Rp. 840.000,00',
+                'status' => 'Lunas'
+            ],
+            [
+                'id' => 7,
+                'name' => 'Audrey Evelyn',
+                'class' => '12 TKJ 1',
+                'tuition' => 'Rp. 840.000,00',
+                'status' => 'Belum Lunas'
+            ],
+            [
+                'id' => 8,
+                'name' => 'Axelle Questine F. T.',
+                'class' => '12 TKJ 2',
+                'tuition' => 'Rp. 840.000,00',
+                'status' => 'Lunas'
+            ],
+            [
+                'id' => 9,
+                'name' => 'Billy',
+                'class' => '12 TKJ 1',
+                'tuition' => 'Rp. 840.000,00',
+                'status' => 'Belum Lunas'
+            ],
+            [
+                'id' => 10,
+                'name' => 'Britania Fisichella',
+                'class' => '12 TKJ 2',
+                'tuition' => 'Rp. 840.000,00',
+                'status' => 'Lunas'
+            ],
+            [
+                'id' => 11,
+                'name' => 'Charles',
+                'class' => '12 TKJ 1',
+                'tuition' => 'Rp. 840.000,00',
+                'status' => 'Belum Lunas'
+            ],
+            [
+                'id' => 12,
+                'name' => 'Daniel Riady',
+                'class' => '12 TKJ 3',
+                'tuition' => 'Rp. 840.000,00',
+                'status' => 'Lunas'
+            ],
+            [
+                'id' => 13,
+                'name' => 'David Aditya Wijaya',
+                'class' => '12 TKJ 1',
+                'tuition' => 'Rp. 840.000,00',
+                'status' => 'Belum Lunas'
+            ],
+            [
+                'id' => 14,
+                'name' => 'Davin Aurelio Yu',
+                'class' => '12 AKL',
+                'tuition' => 'Rp. 840.000,00',
+                'status' => 'Lunas'
+            ],
+            [
+                'id' => 15,
+                'name' => 'Eduardo Gunawan',
+                'class' => '12 TKJ 1',
+                'tuition' => 'Rp. 840.000,00',
+                'status' => 'Belum Lunas'
+            ],
+            [
+                'id' => 16,
+                'name' => "Edward Cornelius",
+                "class" => "12 TKJ 2",
+                "tuition" => "Rp. 840.000,00",
+                "status" => "Lunas"
+            ],
+            [
+                'id' => 17,
+                'name' => "Elvan Emmanuel Firdellia",
+                "class" => "12 TKJ 1",
+                "tuition" => "Rp. 840.000,00",
+                "status" => "Belum Lunas"
+            ],
+            [
+                'id' => 18,
+                'name' => "Enrico Samura",
+                "class" => "12 BiD",
+                "tuition" => "Rp. 840.000,00",
+                "status" => "Lunas"
+            ],
+            [
+                'id' => 19,
+                'name' => "Ericxander",
+                "class" => "12 TKJ 1",
+                "tuition" => "Rp. 840.000,00",
+                "status" => "Belum Lunas"
+            ],
+            [
+                'id' => 20,
+                'name' => "Ethan Francis",
+                "class" => "12 TKJ 2",
+                "tuition" => "Rp. 840.000,00",
+                "status" => "Lunas"
+            ],
+            [
+                'id' => 21,
+                'name' => "Gevin Lionel",
+                "class" => "12 TKJ 3",
+                "tuition" => "Rp. 840.000,00",
+                "status" => "Belum Lunas"
+            ],
+            [
+                'id' => 22,
+                'name' => "Gracia Queen Anastacia",
+                "class" => "12 AKL",
+                "tuition" => "Rp. 840.000,00",
+                "status" => "Lunas"
+            ],
+            [
+                'id' => 23,
+                'name' => "Juan Felix Kantoro",
+                "class" => "12 TKJ 1",
+                "tuition" => "Rp. 840.000,00",
+                "status" => "Belum Lunas"
+            ],
+            [
+                'id' => 24,
+                'name' => "Justin Geraldo",
+                "class" => "12 TKJ 3",
+                "tuition" => "Rp. 840.000,00",
+                "status" => "Lunas"
+            ],
+            [
+                'id' => 25,
+                'name' => "Louis Chai",
+                "class" => "12 TKJ 1",
+                "tuition" => "Rp. 840.000,00",
+                "status" => "Belum Lunas"
+            ],
+            [
+                'id' => 26,
+                'name' => "Michael Robert Yandi",
+                "class" => "12 BiD",
+                "tuition" => "Rp. 840.000,00",
+                "status" => "Lunas"
+            ],
+            [
+                'id' => 27,
+                'name' => "Nicholas Richie Rainhart",
+                "class" => "12 TKJ 1",
+                "tuition" => "Rp. 840.000,00",
+                "status" => "Belum Lunas"
+            ],
+            [
+                'id' => 28,
+                'name' => "Nichole Angelly",
+                "class" => "12 TKJ 2",
+                "tuition" => "Rp. 840.000,00",
+                "status" => "Lunas"
+            ],
+            [
+                'id' => 29,
+                'name' => "Ryo Marvel",
+                "class" => "12 AKL",
+                "tuition" => "Rp. 840.000,00",
+                "status" => "Belum Lunas"
+            ],
+            [
+                'id' => 30,
+                'name' => "Silvio Christian Lim",
+                "class" => "12 BiD",
+                "tuition" => "Rp. 840.000,00",
+                "status" => "Lunas"
+            ],
+            [
+                'id' => 31,
+                'name' => "Vido Faresky",
+                "class" => "12 TKJ 1",
+                "tuition" => "Rp. 840.000,00",
+                "status" => "Belum Lunas"
+            ],
+            [
+                'id' => 32,
+                'name' => "Vinno Alvian Chow",
+                "class" => "12 TKJ 2",
+                "tuition" => "Rp. 840.000,00",
+                "status" => "Lunas"
+            ],
+            [
+                'id' => 33,
+                'name' => "Vincent William",
+                "class" => "12 TKJ 1",
+                "tuition" => "Rp. 840.000,00",
+                "status" => "Belum Lunas"
+            ],
+            [
+                'id' => 34,
+                'name' => "Virgilio Lawrence",
+                "class" => "12 TKJ 2",
+                "tuition" => "Rp. 840.000,00",
+                "status" => "Lunas"
+            ],
+            [
+                'id' => 35,
+                'name' => "Yansen",
+                "class" => "12 TKJ 3",
+                "tuition" => "Rp. 840.000,00",
+                "status" => "Belum Lunas"
+            ]
+        ];
         return view('Admin.index', compact('title', 'students'));
     }
 
--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -13,9 +13,84 @@ class StudentController extends Controller
 
     public function show($id)
     {
-        return view('Students.show', ['id' => $id]);
-    }
+        $title = "SPPku - Halaman Profile Siswa";
 
+        $student = [[
+            'profile' => [
+                'id'          => $id,
+                'name'        => 'Alfredy Rudi',
+                'nisn'        => '0091237740',
+                'nis'         => '7740',
+                'class'       => '12 TKJ 1',
+                'major'     => 'Teknik Komputer dan Jaringan',
+                'school_year' => '2026/2027',
+            ],
+            'account' => [
+                'password'       => '********',
+                'email'          => 'alfredy.rudi@gmail.com',
+                'phone'          => '081234567890',
+                'last_login'     => '26 Maret 2026',
+                'account_status' => 'Aktif',
+            ],
+            'attendance' => [
+                'warning'    => 1, // Pelanggaran
+                'sick'       => 2, // Sakit
+                'permission' => 1, // Izin
+                'late'       => 0, // Terlambat
+            ],
+        ],
+        [
+            'profile' => [
+                'id'          => $id,
+                'name'        => 'Alice Holly Kristy',
+                'nisn'        => '0091237741',
+                'nis'         => '7741',
+                'class'       => '12 AKL',
+                'major'     => 'Akuntansi dan Keuangan Lembaga',
+                'school_year' => '2026/2027',
+            ],
+            'account' => [
+                'password'       => '********',
+                'email'          => 'alice.holly@gmail.com',
+                'phone'          => '081234567891',
+                'last_login'     => '26 Maret 2026',
+                'account_status' => 'Aktif',
+            ],
+            'attendance' => [
+                'warning'    => 0,
+                'sick'       => 1,
+                'permission' => 0,
+                'late'       => 2,
+            ],
+        ],
+        [
+            'profile' => [
+                'id'          => $id,
+                'name'        => 'Amos Mikhael',
+                'nisn'        => '0091237742',
+                'nis'         => '7742',
+                'class'       => '12 TKJ 2',
+                'major'     => 'Teknik Komputer dan Jaringan',
+                'school_year' => '2026/2027',
+            ],
+            'account' => [
+                'password'       => '********',
+                'email'          => 'amos.mikhael@gmail.com',
+                'phone'          => '081234567892',
+                'last_login'     => '26 Maret 2026',
+                'account_status' => 'Aktif',
+            ],
+            'attendance' => [
+                'warning'    => 2,
+                'sick'       => 0,
+                'permission' => 1,
+                'late'       => 1,
+            ],
+        ]
+    ];
+
+        return view('Students.show', compact('title', 'id', 'student'));
+    }
     public function payments($id)
     {
         return view('Students.payments', ['id' => $id]);
