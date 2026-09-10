d70e9411410cecbb75af11b1932162ba21c45929
AliceeHk <alicehk1218@gmail.com>
2026-09-10 10:21:16 +0700

    feat: implement students index page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,20 +10,19 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="flex min-h-screen flex-col bg-[#F7F6F2] text-slate-700">
+<body class="flex h-screen flex-col bg-[#F7F6F2] text-slate-700"> 
     {{-- Header Start --}}
-    @include('layouts.partials.header')
-    {{-- Header End --}}
-
-    <div class="mx-auto flex w-full flex-1">
-        {{-- Sidebar Start --}}
+     @include('layouts.partials.header')
+     {{-- Header End --}}
+     
+     {{-- Main Area --}} 
+     <div class="mx-auto flex w-full flex-1 min-h-0">
+        {{-- Sidebar Start --}} 
         @include('layouts.partials.sidebar')
         {{-- Sidebar End --}}
-
-        {{-- Content Start --}}
-        <main class="flex-1 px-10 py-5 bg-[#F3F5FF] overflow-hidden">
-            @yield('content')
-        </main>
+        
+        {{-- Content Start --}} 
+        <main class="flex-1 min-h-0 overflow-y-auto bg-[#F3F5FF] px-10 py-5"> @yield('content') </main>
         {{-- Content End --}}
     </div>
 
